dc2521d0187f79cc98bfd95d767829801bac8ce6
RahulRaj1417 <72130755+RahulRaj1417@users.noreply.github.com>
2021-05-10 15:50:24 +0530

    Update setup-termux.sh

--- setup-termux.sh
+++ setup-termux.sh
@@ -1,4 +1,22 @@
 !/data/data/com.termux/files/usr/bin/bash
 #just a simple script for setup my termux settings
 
-echo ""
+echo "Setting up storage by giving storage permission"
+sleep 3s
+if [[ -d ~/storage ]]
+then
+    termux-setup-storage
+fi
+mkdir ~/.termux 2>/dev/null
+if [[ -r ~/Set-up-my-termux/termux.properties ]]
+then
+    cp ~/Set-up-my-termux/termux.properties ~/.termux/termux.properties
+    termux-reload-settings
+else
+    echo "can't find properties file"
+fi
+echo "All Done"
+exit 0
+
+
+
