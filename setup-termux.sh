e8b6a6384eb53b0d3a3b704324569d032f0b14b4
Rrhul <72130755+Rrhul0@users.noreply.github.com>
2021-05-11 13:23:37 +0530

    Update setup-termux.sh

--- setup-termux.sh
+++ setup-termux.sh
@@ -15,6 +15,34 @@ then
 else
     echo "can't find properties file"
 fi
+
+echo "Installing ZSH and its required packages"
+pkg update
+pkg install zsh
+mkdir ~/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+#zsh-completions
+echo "fpath=(~/.zsh/zsh-completions/src $fpath)" > ~/.zshrc
+
+#copy zshrc to .zshrc file
+cat ~/.zsh/zshrc >> ~/.zshrc
+
+#zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+#zsh-history-substring-search
+echo 'source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh' >> ~/.zshrc
+
+#zsh-syntax-highlighting
+echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+#changing shell for user
+chsh -s zsh
+
 echo "All Done"
 exit 0
 
